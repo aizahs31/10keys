c96043190c014663c0ffc85be99cd4c8d76f6ca1
Shazia / Ryze <shaziafarheen10@gmail.com>
2026-03-14 04:42:17 +0530

    Convert serial to keystroke

---
+++ README.md
@@ -0,0 +1,18 @@
+# 10 keys - a keyboard with *drumrole* 10 KEYS!!!
+
+To run this project, you need:
+1. Arduino Mega (or any other Arduino)
+2. 10 keys (duh)
+3. Brownie points for "shift"ing
+
+# To run:
+### 1. Upload the ino sketch to the microcontroller
+### 2. Run python script to convert serial input to key strokes
+- Install the required libraries
+```
+pip install pyserial pynput
+```
+- Run the script
+```
+python keystroke.py
+```
---
+++ keystroke.py
@@ -0,0 +1,33 @@
+import serial
+import time
+from pynput.keyboard import Controller, Key
+
+PORT = "COM3"
+BAUD = 9600
+
+ser = serial.Serial(PORT, BAUD, timeout=1)
+time.sleep(2)
+
+keyboard = Controller()
+
+print("Listening...")
+
+while True:
+    raw = ser.readline()
+
+    if not raw:
+        continue
+
+    line = raw.decode("utf-8").replace("\r","").replace("\n","")
+
+    # ignore garbage
+    if line == "":
+        continue
+
+    print("Received:", repr(line))
+
+    if line == " ":
+        keyboard.press(Key.space)
+        keyboard.release(Key.space)
+    elif len(line) == 1:
+        keyboard.type(line)
