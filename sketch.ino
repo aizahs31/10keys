138c707d01b5f6a656c4dfef96b2d6009b46cefa
Shazia / Ryze <shaziafarheen10@gmail.com>
2026-03-14 03:47:55 +0530

    All alphabets!

--- sketch.ino
+++ sketch.ino
@@ -3,12 +3,13 @@ const int pins[10] = {11,10,9,8,7,6,5,4,3,2};
 char singleLetters[10] = {'a','b','c','d','e','f','g','h','i','j'};
 char doubleLetters[10] = {'k','l','m','n','o','p','q','r','s','t'};
 
-unsigned long firstPressTime[10];
 bool waitingSecond[10];
+unsigned long firstPressTime[10];
 
 const int doubleTime = 300;
 
 void setup() {
+
   Serial.begin(9600);
 
   for(int i=0;i<10;i++){
@@ -19,32 +20,78 @@ void setup() {
 
 void loop() {
 
+  bool anyPressed = false;
+
   for(int i=0;i<10;i++){
+    if(digitalRead(pins[i]) == LOW)
+      anyPressed = true;
+  }
 
-    if(digitalRead(pins[i]) == LOW){
+  if(anyPressed){
 
-      delay(20); // debounce
+    bool chord[10] = {0};
 
-      if(digitalRead(pins[i]) == LOW){
+    while(true){
 
-        unsigned long now = millis();
+      bool stillPressed = false;
 
-        if(waitingSecond[i] && (now - firstPressTime[i] < doubleTime)){
-          Serial.println(doubleLetters[i]);
-          waitingSecond[i] = false;
+      for(int i=0;i<10;i++){
+        if(digitalRead(pins[i]) == LOW){
+          chord[i] = true;
+          stillPressed = true;
         }
-        else{
-          waitingSecond[i] = true;
-          firstPressTime[i] = now;
-        }
-
-        while(digitalRead(pins[i]) == LOW);
       }
+
+      if(!stillPressed) break;
     }
 
-    if(waitingSecond[i] && (millis() - firstPressTime[i] > doubleTime)){
+    decodeChord(chord);
+    delay(30);
+  }
+
+  for(int i=0;i<10;i++){
+    if(waitingSecond[i] && millis() - firstPressTime[i] > doubleTime){
       Serial.println(singleLetters[i]);
       waitingSecond[i] = false;
+    }
+  }
+}
+
+void decodeChord(bool k[10]){
+
+  int count = 0;
+  int index = -1;
+
+  for(int i=0;i<10;i++){
+    if(k[i]){
+      count++;
+      index = i;
+    }
+  }
+
+  if(count == 2){
+
+    if((k[0] && k[1])){ Serial.println("u"); return; }
+    if((k[1] && k[2])){ Serial.println("v"); return; }
+    if((k[2] && k[3])){ Serial.println("w"); return; }
+    if((k[6] && k[7])){ Serial.println("x"); return; }
+    if((k[7] && k[8])){ Serial.println("y"); return; }
+    if((k[8] && k[9])){ Serial.println("z"); return; }
+    if((k[4] && k[5])){ Serial.println(" "); return; }
+
+  }
+
+  if(count == 1){
+
+    unsigned long now = millis();
+
+    if(waitingSecond[index] && now - firstPressTime[index] < doubleTime){
+      Serial.println(doubleLetters[index]);
+      waitingSecond[index] = false;
+    }
+    else{
+      waitingSecond[index] = true;
+      firstPressTime[index] = now;
     }
   }
 }
